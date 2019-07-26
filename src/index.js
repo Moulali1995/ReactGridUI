e85d403f3a4f222cec3066877d6c5dc5154ca195
MOULALI M <moulalim.1995@gmail.com>
2019-07-26 16:55:54 +0530

    added grid feature

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./styles.css";
+// Main component for inputs and Box component calls.
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -13,10 +14,33 @@ class Main extends React.Component {
     };
     this.handleInputChange = this.handleInputChange.bind(this);
   }
+  // handle the input change for different attributes like no. of rows,columns,padding and margin.
   handleInputChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
   render() {
+    let rows = [];
+    // columnsarr to get the number of columns
+    let columnsarr = this.state.columnsstring.split(",");
+    console.log(columnsarr);
+    // Loop for returning the grid of rows and columns
+    for (var i = 0; i < this.state.noofrows; i++) {
+      let ele = columnsarr[i];
+      let noofcolumns = parseInt(ele, 10);
+      console.log(noofcolumns);
+      for (var j = 0; j < noofcolumns; j++) {
+        rows.push(
+          <div>
+            <Box specs={this.state} />
+          </div>
+        );
+      }
+      rows.push(
+        <div>
+          <br />
+        </div>
+      );
+    }
     return (
       <div className="Main">
         <h2>REACT GRID SYSTEM </h2>
@@ -47,18 +71,25 @@ class Main extends React.Component {
         />
         <br />
         <br />
-        {console.log(this.state)}
-
-        <Box specs={this.state} />
-        <Box specs={this.state} />
+        {rows.map(ele => {
+          return ele;
+        })}
       </div>
     );
   }
 }
+// Box component to return the box elements which are to be rendered in the grid.
 function Box(props) {
-  const margin = props.specs.guttercol;
-  const boxobj = { height: "100px", border: "5px solid", margin: margin };
-  console.log(boxobj);
+  const { gutterrow, guttercol } = props.specs;
+  // style object to provide the style attributes for the box
+  const boxobj = {
+    height: "100px",
+    border: "5px solid",
+    backgroundColor: "brown",
+    margin: guttercol,
+    padding: gutterrow
+  };
+
   return (
     <div className="Box" style={boxobj}>
       <h1>100px Height Box</h1>
